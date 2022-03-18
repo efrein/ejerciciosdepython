75c879d075e80ec6ca019131d67d5ba717863bf0
efrein <eiguedez@misena.edu.co>
2022-03-18 11:57:33 -0500

    actualizacion 2
    efrein paloma

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+radio= 5
+volumen= 4/3 *3.1416 *(radio**3)
+print ("el volumen de la esfera es:", round (volumen))
---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+capital=10000
+interes=3.5
+años=7
+valor= (10000* (1+0.035)**7)
+print ("el futuro valor del dinero es:", round (valor))
